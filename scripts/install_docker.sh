727b19e09eb76e944e343f9b7443fc34b1e71148
mykola <lavirozp@gmail.com>
2016-04-26 13:37:02 +0300

    init

---
+++ scripts/install_docker.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# install docker
+wget -qO- https://get.docker.com/ | sh
+
+
+# install docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.7.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+
+echo "";
+echo "";
+echo "";
+echo "======== Installed =========";
+docker -v
+docker-compose --version
