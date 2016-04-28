33c07dba5cdd0b9223466a9175097805b6027292
mykola <lavirozp@gmail.com>
2016-04-28 16:43:35 +0300

    scripts for yii

--- run.sh
+++ run.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 
-docker-compose up
+docker-compose up -d
+
+
+# if yii2 exist, then update it
+if [ -f ./www/_protected/yii ]; then
+    sleep 1
+    ./scripts/yii/update_yii
+fi
+
